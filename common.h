c7800d47424df71a3ef1800c2dcb31f1973001ed
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2025-12-29 20:55:15 -0300

    urandom part of common.h + made my own example easier to read for myself.

--- common.h
+++ common.h
@@ -1,7 +1,23 @@
+#ifndef _COMMON_H
+#define _COMMON_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+
+void urandom(uint8_t* out, size_t szout) {
+    FILE* fd = fopen("/dev/urandom", "r");
+    fread(out, 1, szout, fd);
+    fclose(fd);
+}
+
+bool coinflip(void) {
+    uint8_t buf[1];
+    urandom(buf, 1);
+
+    return buf[0] > 127;
+}
 
 void hexdump(void* data, size_t size) {
     for(size_t i = 0; i < size; ++i) {
@@ -9,3 +25,5 @@ void hexdump(void* data, size_t size) {
     }
     printf("\n");
 }
+
+#endif
--- testblock.c
+++ testblock.c
@@ -1,4 +1,5 @@
 #include "braille.h"
+#include "color/color.h"
 #include "i3bar.h"
 #include <locale.h>
 #include <stdint.h>
@@ -9,60 +10,68 @@
 
 #include "common.h"
 
-void urandom(uint8_t* out, size_t szout) {
-    FILE* fd = fopen("/dev/urandom", "r");
-    fread(out, 1, szout, fd);
-    fclose(fd);
-}
-
-bool coinflip() {
-    uint8_t buf[1];
-    urandom(buf, 1);
-
-    return buf[0] > 127;
-}
-
 int main(void) {
 
     setlocale(LC_ALL, "");
     int iters = 0;
     while(1) {
         iters++;
-        uint8_t rbuf[128];
-        urandom(rbuf, 128);
 
-        wchar_t wbuf[128];
-        size_t  w = braille_write(wbuf, 128, rbuf, 128);
-        // wprintf(L"Wrote %zu\n", w);
-        wbuf[64] = L'\0';
-        // wprintf(L"%ls\n", wbuf);
+        size_t random_count = 150;
+
+        uint8_t random_bytes[random_count];
+        urandom(random_bytes, random_count);
+
+        wchar_t random_braille[random_count];
+        braille_write(random_braille, random_count, random_bytes, random_count);
+
+        // NOTE:
+        // This was the part that broke it. The random braille needs a null
+        // in order for sprintf to be able to know how much of it to write
+        // to the block_text_mb buffer.
+        random_braille[random_count] = L'\0';
 
         i3bar_block_t block;
         i3bar_block_init(&block);
 
-        char full_text[4096];
-        memset(full_text, 0, 4096);
+        size_t block_size_mb =
+          (sizeof(wchar_t) * random_count) + sizeof(wchar_t) * 256;
 
-        int wr = sprintf(full_text,
-                         "<b><span color='#00FF00'>   %ls   </span></b>",
-                         wbuf);
-        // wprintf(L"%d\n%s\n", wr, full_text);
-        wchar_t wfull[127];
-        memset(wfull, 0, 127 * sizeof(wchar_t));
+        char block_text_mb[block_size_mb];
+        memset(block_text_mb, 0, block_size_mb);
 
-        // full_text[128] = '\0';
-        mbstowcs(wfull, full_text, 128);
+        // Write the formatted Pango string containing the random braille to
+        // the block_text_mb multibyte string buffer. This has to be a char
+        // and not wchar_t buffer because I have not found a "wsprintf"
+        sprintf(block_text_mb,
+                "<b><span color='%s'>%ls</span></b>",
+                rgbx(GREEN),
+                random_braille);
 
-        // wprintf(L"FULL\n%ls\n", wfull);
+        // Using the same size as that of the multibyte is sure to give us
+        // more space than the multibyte buffer because these will be treated
+        // as wchar_t's, so it's sizeof(wchar_t) * block_size_mb due to the
+        // type of this buffer being a wchar_t[].
+        size_t  block_size_w = block_size_mb;
+        wchar_t block_text_w[block_size_w];
+        memset(block_text_w, 0, block_size_w);
 
-        block.full_text = wfull;
+        // Convert: block_text_mb -> block_text_w
+        mbstowcs(block_text_w, block_text_mb, block_size_w);
+
+        // Put a nullbyte at a reasonable location.
+        // block_text_w[512] = L'\0';
+
+        // printf("%s\n", block_text_mb);
+
+        block.full_text = block_text_w;
         // block.full_text =
         //   L"<b><span foreground='#0FfF00'>H \"Hi\"  ello
         //   ⠅⠆⠇⡀⡁⡂ " L"  " "World World</span></b>";
         // block.short_text    = "HW";
-        block.color         = "#FFFF00";
-        block.background    = "#000000";
-        block.border        = "#FFF000";
+        // block.color         = "#FFFF00";
+        // block.background    = rgbx(BG_DIM);
+        // block.border        = "#FFF000";
         block.align         = I3B_ALIGN_CENTER;
         block.border_top    = 0;
         block.border_bottom = 0;
@@ -75,11 +84,12 @@ int main(void) {
         block.min_width.type     = I3B_MIN_WIDTH_PIXELS;
         block.min_width.u.pixels = 300;
 
-        block.markup = "pango";
-        // block.separator = false;
+        block.markup                = "pango";
+        block.separator             = false;
+        block.separator_block_width = 0;
 
         i3bar_block_output(&block);
         fflush(stdout);
-        usleep(100000);
+        usleep(500000);
     }
 }
