0da9558299e165b045eb8fc28464cbaa5d061850
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2025-12-29 04:25:45 -0300

    Defined chart bitwise macros

--- braille.h
+++ braille.h
@@ -1,9 +1,30 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <wchar.h>
+
+//       ⣦   ⢰
+//    ⣦
+//
+// left 1          ⣰
+// right 1
+// left 2          ⣤ ⣴ ⣼
+// right 2       ⣠ ⣤ ⣦ ⣧
+// left 3        ⣆ ⣦ ⣶ ⣾
+// right 3       ⣰ ⣴ ⣶ ⣷
+// left 4        ⣇ ⣧ ⣷ ⣿
+// right 4       ⣸ ⣼ ⣾ ⣿
+// equal         ⣿ ⣶ ⣤ ⣀
+
+// const wchar_t
+//     c11 = L'⣀',
+//     c12 = L'⣠',
+//     c13 = L'⣰',
+//     c14 = L'⣸',
+//     c21 =
 
 // clang-format off
 const wchar_t BRAILLE_TABLE[257] = {
@@ -26,6 +47,49 @@ const wchar_t BRAILLE_TABLE[257] = {
     /*F*/ "⢸⢹⢺⢻⢼⢽⢾⢿⣸⣹⣺⣻⣼⣽⣾⣿"
 };
 
+
+// For chart
+#define R4 8 | 128 | 64 | 32 | 16
+#define R3 8 | 128 | 64 | 32
+#define R2 8 | 128 | 64
+#define R1 8 | 128
+#define L1 8 | 128
+#define L2 8 | 128 | 4
+#define L3 8 | 128 | 4 | 2
+#define L4 8 | 128 | 4 | 2 | 1
+
+// clang-format on
+
+/*
+
+
+const wchar_t BRAILLE_TABLE[257] = {
+  L"⠀⠁⠂⠃⠄⠅⠆⠇⡀⡁⡂⡃⡄⡅⡆⡇"
+   "⠈⠉⠊⠋⠌⠍⠎⠏⡈⡉⡊⡋⡌⡍⡎⡏"
+   "⠐⠑⠒⠓⠔⠕⠖⠗⡐⡑⡒⡓⡔⡕⡖⡗"
+   "⠘⠙⠚⠛⠜⠝⠞⠟⡘⡙⡚⡛⡜⡝⡞⡟"
+   "⠠⠡⠢⠣⠤⠥⠦⠧⡠⡡⡢⡣⡤⡥⡦⡧"
+   "⠨⠩⠪⠫⠬⠭⠮⠯⡨⡩⡪⡫⡬⡭⡮⡯"
+   "⠰⠱⠲⠳⠴⠵⠶⠷⡰⡱⡲⡳⡴⡵⡶⡷"
+   "⠸⠹⠺⠻⠼⠽⠾⠿⡸⡹⡺⡻⡼⡽⡾⡿"
+   "⢀⢁⢂⢃⢄⢅⢆⢇⣀⣁⣂⣃⣄⣅⣆⣇"
+   "⢈⢉⢊⢋⢌⢍⢎⢏⣈⣉⣊⣋⣌⣍⣎⣏"
+   "⢐⢑⢒⢓⢔⢕⢖⢗⣐⣑⣒⣓⣔⣕⣖⣗"
+   "⢘⢙⢚⢛⢜⢝⢞⢟⣘⣙⣚⣛⣜⣝⣞⣟"
+   "⢠⢡⢢⢣⢤⢥⢦⢧⣠⣡⣢⣣⣤⣥⣦⣧"
+   "⢨⢩⢪⢫⢬⢭⢮⢯⣨⣩⣪⣫⣬⣭⣮⣯"
+   "⢰⢱⢲⢳⢴⢵⢶⢷⣰⣱⣲⣳⣴⣵⣶⣷"
+   "⢸⢹⢺⢻⢼⢽⢾⢿⣸⣹⣺⣻⣼⣽⣾⣿"
+};
+
+setlocale(LC_ALL, "')
+for(int i=0; i<256; i++) {
+wchar_t c = BRAILLE_TABLE[i];
+printf("%lc\n", c);
+}
+   */
+
+// clang-format off
 /* Read up to sz_src bytes from src and write the braille representation as a
  * wchar_t to dst; this function asserts that sz_dst is less than sz_src.
  *
@@ -39,7 +103,7 @@ static inline size_t braille_write(wchar_t* dst, size_t sz_dst,
                                    uint8_t* src, size_t sz_src)
 { /* clang-format on */
 
-    size_t index     = 0;
+    size_t index = 0;
 
     for(; index < sz_dst; ++index) {
 
@@ -54,6 +118,75 @@ static inline size_t braille_write(wchar_t* dst, size_t sz_dst,
     return index;
 }
 
+static inline void
+  minmax(double* array, size_t sz_array, double** outmin, double** outmax) {
+    double *min = array, *max = array;
+
+    for(size_t i = 0; i < sz_array; ++i) {
+        if(array[i] > *max) {
+            max = &array[i];
+        } else if(array[i] < *min) {
+            min = &array[i];
+        }
+    }
+
+    *outmin = min;
+    *outmax = max;
+}
+
+/* clang-format off */
+static inline size_t braille_inline_chart(wchar_t* dst, size_t sz_dst,
+                                          double*  data, size_t sz_data,
+                                          size_t   chart_length)
+{ /* clang-format on */
+
+    wchar_t chart[chart_length];
+
+    double *min, *max;
+    minmax(data, sz_data, &min, &max);
+
+    for(size_t i = 0; i < sz_data; i++) {
+        double value      = data[i];
+        double normalized = (value - *min) / (*max - *min);
+        double percent    = normalized * 100;
+
+        // Brail is 4 tall max
+
+        // if(percent
+        //
+    }
+}
+
+/*
+
+void minmax(double* array, size_t sz_array, double** outmin, double** outmax) {
+    double *min = array, *max = array;
+
+    for(size_t i = 0; i < sz_array; ++i) {
+        if(array[i] > *max) {
+            max = &array[i];
+        } else if(array[i] < *min) {
+            min = &array[i];
+        }
+    }
+
+    *outmin = min;
+    *outmax = max;
+}
+
+size_t ds = 7;
+   double data[] = { 13.0, 15.124, 50.1234, 19.123, 6.124, 62.234, 10000.0 };
+
+   double *min, *max;
+   minmax(data, ds, &min, &max);
+
+   for(int i =0; i<ds;++i) {
+   double value = data[i];
+        double normalized = (value - *min) / (*max - *min);
+    printf("%lf %lf %lf %lf\n", data[i], *min,*max, data[i] / *max);
+    printf("Mapped: %lf %lf\n", 4.0 / (data[i] / *max)*100, 4.0 / (data[i] /
+*max))  ; printf("Norm :%lf\n", (normalized*100.0)/4.0);
+   }
 
 
 
@@ -61,11 +194,4 @@ static inline size_t braille_write(wchar_t* dst, size_t sz_dst,
 
 
 
-
-
-
-
-
-
-
-
+ */
