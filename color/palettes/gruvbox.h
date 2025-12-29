44c16f715f5004a7502049429a9f424f9cea7807
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2025-12-29 20:54:30 -0300

    Added BG_DIM to Gruvbox, which I have set as my BG in i3's bar config.

--- color/palettes/gruvbox.h
+++ color/palettes/gruvbox.h
@@ -1,7 +1,8 @@
 #define PALETTE GRUVBOX
 
 // clang-format off
-#define BG     (Color) { 40,   40,  40  }
+#define BG     (Color) { 40,   40,  40  } 
+#define BG_DIM (Color) { 36,   36,  36  } 
 #define FG     (Color) { 235,  219, 178 }
 #define FG_DIM (Color) { 168,  153, 132 }
 #define BLUE   (Color) { 69,   133, 136 }
