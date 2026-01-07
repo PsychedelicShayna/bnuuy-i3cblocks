f24b7f934778a7ab213335c2d8294693c0dbf6ef
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2026-01-06 21:47:19 -0300

    Per-character braille, but waits for two samples, needs work

--- cpu.c
+++ cpu.c
@@ -1,21 +1,27 @@
+#include <assert.h>
 #include <locale.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <wchar.h>
 
 #ifndef CHARTSIZE
-#define CHARTSIZE 33
+#define CHARTSIZE 32
 #endif
 
 #include "braille.h"
 #include "color/color.h"
 #include "i3bar.h"
+#include "pango2.h"
+
+// #include "pango.h"
 
 #ifndef USLEEPFOR
 #define USLEEPFOR 1000000
 #endif
+#define USLEEPFOR 250000
 
 typedef struct {
     int64_t t_user_time;
@@ -26,13 +32,13 @@ typedef struct {
     int64_t t_interrupt;
     int64_t t_soft_interrupt;
     int64_t t_stolen;
-
     int64_t t_guest; // VM Memory
     int64_t t_guest_nice;
 } proc_stat_t;
 
 void minmax(int64_t* a, int64_t* b, int64_t** out_min, int64_t** out_max)
 {
+
     if(*a >= *b) {
         *out_max = a;
         *out_min = b;
@@ -129,27 +135,45 @@ void output(void)
     GradientStep* color_gradient = Gradient(
       Threshold(10.0, GREEN), Threshold(50.0, ORANGE), Threshold(100.0, RED));
 
-    double history[(CHARTSIZE) * 2];
-    Color  chistory[sizeof(history) / sizeof(history[0])];
+    // history size must be double the chart size because the chart represents
+    // two datapoints in history for every one braille chracter in the chart.
+    size_t HISTORYSIZE = CHARTSIZE * 2;
+
+    double history[HISTORYSIZE];
+    Color  chistory[HISTORYSIZE];
 
     for(size_t i = 0; i < sizeof(history) / sizeof(history[0]); i++) {
         chistory[i] = GREEN;
         history[i]  = 0.0;
     }
 
-    wchar_t chart[CHARTSIZE];
-    memset(chart, L'⣀', sizeof(chart));
-
-    write_braille_chart(chart,
-                        sizeof(chart) / sizeof(chart[0]),
-                        history,
-                        sizeof(history) / sizeof(history[0]),
-                        -1,
-                        -1);
-
     i3bar_block_t block;
     i3bar_block_init(&block);
     block.markup = "pango";
+
+#ifdef TODO_REPLACE_THIS
+    // String builder for pango markup. We will use this to write to the
+    // block.full_text after programatically decorating the output with
+    // Pango markup. Namely, coloring each individual chart character
+    // differently according to the color in chistory.
+
+    pango_string_builder psb = pangosb_create(8);
+#endif
+
+    wchar_t chart[CHARTSIZE];
+    memset(chart, L'⣀', sizeof(chart));
+
+    size_t   largebuf_size  = 16383;
+    wchar_t* largebuf_pango = malloc(largebuf_size);
+    memset((void*)largebuf_pango, 0, largebuf_size);
+
+    panspan braille_span;
+    panspan_init(&braille_span);
+    wchar_t pre_outbuf[256];
+
+    size_t test = sizeof(chistory);
+
+    int pair_offset = 0;
 
     while(1) {
         double usage     = cpu_usage();
@@ -158,40 +182,126 @@ void output(void)
 
         memmove(history, &history[1], sizeof(history) - sizeof(double));
         memmove(chistory, &chistory[1], sizeof(chistory) - sizeof(Color));
-
         history[sizeof(history) / sizeof(history[0]) - 1]    = usage;
         chistory[sizeof(chistory) / sizeof(chistory[0]) - 1] = color;
 
+        pair_offset ^= 1;
         write_braille_chart(&chart[0],
                             (sizeof(chart) / sizeof(chart[0])),
-                            history,
-                            (sizeof(history) / sizeof(history[0])),
+                            &history[pair_offset],
+                            (sizeof(history) / sizeof(history[0]))-pair_offset,
                             -1,
                             -1);
 
-        const char*    color_hex = rgbx(color);
-        static wchar_t full_text[256];
+        // 6 good, 6 offby1, 7, good 7 offby1, it's the uneven index, i*2
+        // is not a good strategy.
 
-        swprintf(full_text,
-                 sizeof(full_text) - 1,
-                 L"<span color=\"%s\"> "
-                 L"%ls %.2lf%% "
-                 L"%02.2lfGHz   </span>",
-                 color_hex,
-                 &chart[0],
+        for(size_t i = 0; i < CHARTSIZE; i++) {
+            // size_t  chart_idx        = i / 2;
+
+            size_t s0 = pair_offset + 2 * i;
+            size_t s1 = s0 + 1;
+
+            if(s0 >= HISTORYSIZE)
+                break;
+            double v0 = history[s0];
+            double v1 = (s1 < HISTORYSIZE) ? history[s1] : v0;
+
+            Color c0 = chistory[s0];
+            Color c1 = (s1 < HISTORYSIZE) ? chistory[s1] : c0;
+
+            Color chosen =
+              (v0 >= v1) ? c0 : c1; // history[s0] >= history[s1] ? c1 : c2;
+                                    //
+            if(i >= CHARTSIZE - 1) {
+                chosen = color;
+            }
+
+            // if(i >= CHARTSIZE - 1) {
+            //     chosen = color;
+            // }
+
+            // Color *min = &color_a, *max = &color_b;
+            //
+            // if((min->r + min->g + min->b) > (max->r + max->g + max->b)) {
+            //     max = &color_a;
+            //     min = &color_b;
+            // }
+
+            wchar_t braille_chars[2] = { chart[i], L'\0' };
+
+            braille_span.foreground = rgbx(chosen);
+
+            memset(pre_outbuf, 0, sizeof(pre_outbuf));
+            pango_format(pre_outbuf,
+                         sizeof(pre_outbuf) / sizeof(pre_outbuf[0]),
+                         braille_chars,
+                         PSBT_NULL,
+                         &braille_span);
+
+            wcslcat(largebuf_pango,
+                    pre_outbuf,
+                    largebuf_size / sizeof(largebuf_pango[0]));
+
+            // wprintf(L"Color A: %s, Color B: %s for BC: '%lc'\n",
+            //         rgbx(color_a),
+            //         rgbx(color_b),
+            //         braille_char);
+        }
+
+        memset(pre_outbuf, 0, sizeof(pre_outbuf));
+        const char* color_hex = rgbx(color);
+
+        swprintf(pre_outbuf,
+                 sizeof(pre_outbuf) - 1,
+                 L" %05.2lf%% %02.2lfGHz  ",
+                 // &chart[0],
                  usage,
                  frequency);
 
-        block.full_text = full_text;
+        wchar_t outbuf[1024];
+        memset(outbuf, 0, 1024 * sizeof(wchar_t));
+
+        panspan ps;
+        panspan_init(&ps);
+        ps.foreground = (char*)color_hex;
+
+        pango_format(outbuf, 1024, pre_outbuf, PSBT_NULL, &ps);
+
+        wcslcat(
+          largebuf_pango, outbuf, largebuf_size / sizeof(largebuf_pango[0]));
+
+#ifdef TODO_REPLACE_THIS
+        // Write the full_text string to the pango string builder, to make
+        // it a colored span using the foreground color determined earlier.
+        pangosb_wpush(&psb,
+                      full_text,
+                      PSBT_NULL,
+                      &((pango_span) { .foreground = (char*)color_hex }));
+
+        // Then we extract the last string from the pango string builder.
+
+        wchar_t final_buf[pangosb_size_last(&psb) + 1];
+        memset(final_buf, 0, sizeof(final_buf));
+        pangosb_get_last(&psb, final_buf, sizeof(final_buf));
+#endif
+
+        // Then set it as the value for the JSON full_text ky and output it.
+        block.full_text = largebuf_pango;
         i3bar_block_output(&block);
+
+        memset(largebuf_pango, 0, largebuf_size);
 
         // We don't sleep here, since the sampling algorithm includes usleep
         // in order to get two relative samples to calculate usgae.
     }
+
+    free(largebuf_pango);
 }
 
 int main(void)
 {
+
     setlocale(LC_ALL, "");
     output();
     return EXIT_SUCCESS;
