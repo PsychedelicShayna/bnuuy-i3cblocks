ddf509e03801edd433797e544d85c671870936f1
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2026-01-09 06:09:21 -0300

    More helper macros and cuter names~
    pomf! wpomf! PAT_!  *muehehehe*
    usagi = usage

--- pango2.h
+++ pango2.h
@@ -135,17 +135,18 @@ typedef struct {
     float scale;
 } panspan;
 
+// `*pat pat*`
 typedef enum {
-    PSBT_NULL = 0x0000,
-    PSBT_ITAL = 0x0001,
-    PSBT_BOLD = 0x0002,
-    PSBT_ULIN = 0x0004,
-    PSBT_STRK = 0x0008,
-    PSBT_MONO = 0x0010,
-    PSBT_SUB  = 0x0020,
-    PSBT_SUP  = 0x0040,
-    PSBT_SMAL = 0x0080,
-    PSBT_BIG  = 0x0100
+    PAT_NULL = 0x0000,
+    PAT_ITAL = 0x0001,
+    PAT_BOLD = 0x0002,
+    PAT_ULIN = 0x0004,
+    PAT_STRK = 0x0008,
+    PAT_MONO = 0x0010,
+    PAT_SUB  = 0x0020,
+    PAT_SUP  = 0x0040,
+    PAT_SMAL = 0x0080,
+    PAT_BIG  = 0x0100
 } pantag;
 
 void panspan_init(panspan* pspan)
@@ -163,18 +164,8 @@ void panspan_init(panspan* pspan)
     pspan->scale          = PANGO_DEFAULT_FLT;
 }
 
-#define panf(span, tags, format, ...)                   \
-    ({                                                  \
-        panspan        s = span;                        \
-        static wchar_t buffer[sizeof(wchar_t) * 128];   \
-        static wchar_t buffer2[sizeof(wchar_t) * 128];  \
-        memset(buffer, 0, sizeof(wchar_t) * 128);       \
-        memset(buffer2, 0, sizeof(wchar_t) * 128);      \
-        swprintf(buffer, 128, L"" format, __VA_ARGS__); \
-        pango_format(buffer2, 128, buffer, tags, &s);   \
-        buffer2;                                        \
-    })
-
+// Creates a panspan with the fields in `__VA_ARGS__` set, and all other
+// fields set to their default values. e.g. mkspan(.foreground="red");
 #define mkspan(...)                                                          \
     (panspan)                                                                \
     {                                                                        \
@@ -185,6 +176,104 @@ void panspan_init(panspan* pspan)
         .rise = PANGO_DEFAULT_INT, .letter_spacing = PANGO_DEFAULT_INT,      \
         .scale = PANGO_DEFAULT_FLT, __VA_ARGS__                              \
     }
+
+// Returns a modified copy of span with the fields in `__VA_ARGS__` changed
+// from their original values, with unchanged fields retaining their original
+// values. Within the macro, the original span can also be be referred to in
+// `__VA_ARGS__` as `ps` so that you an modify fields relative to their values
+// on the original `ps.fieldname`, e.g. modspan(ps, .size = ps.size + 10);
+#define modspan(__base_span, ...)                                          \
+    ({                                                                     \
+        panspan ps      = __base_span;                                     \
+        panspan newspan = (panspan) { .foreground     = ps.foreground,     \
+                                      .background     = ps.background,     \
+                                      .font_desc      = ps.font_desc,      \
+                                      .size           = ps.size,           \
+                                      .weight         = ps.weight,         \
+                                      .style          = ps.style,          \
+                                      .underline      = ps.underline,      \
+                                      .strikethrough  = ps.strikethrough,  \
+                                      .rise           = ps.rise,           \
+                                      .letter_spacing = ps.letter_spacing, \
+                                      .scale          = ps.scale,          \
+                                      __VA_ARGS__ };                       \
+        newspan;                                                           \
+    })
+
+// Define the buffer size for wpomf macro if not already defined. Allows user
+// to override the default size of 128 if needed prior to include.
+#ifndef WPOMF_BUFFER_SIZE
+#define WPOMF_BUFFER_SIZE 128
+#endif /* ifndef WPOMF_BUFFER_SIZE */
+
+// A wrapper around pango_format which returns a formatted wide string based
+// on the input format and `__VA_ARGS__`, with the specified `tags` and `span`
+// whiach follows the same syntax as pango_format. The returned wchar_t* should
+// not be freed by the caller, it's stored in a static buffer. The main user
+// of this macro is i3bcat to generate and conctenate several pango formatted
+// strings inline to a single i3bar block's full_text field. This can be used
+// together with mkspan or modspan to create spans inline as well. The format
+// string is wide, but L"" prefix is added automatically, so normal string
+// literals can be used as well, including those from other mascros.
+//
+// Limits: The formatted string is limited to WPOMF_BUFFER_SIZE wide characters
+// (including the null terminator). If the formatted string exceeds this size,
+// it will be truncated. If more space is needed, increase WPOMF_BUFFER_SIZE
+// prior to including this header.
+//
+// Usagi: wpomf(mkspan(.foreground="red"), PSBT_BOLD, "Hello %s", "World");
+// wpomf = (w)ide, (p)ang(o) (m)arkup (f)ormat~ cute!! even if I did force
+// the name a little! hehe
+
+#define wpomf(span, tags, format, ...)                                \
+    ({                                                                \
+        panspan        s = span;                                      \
+        static wchar_t buffer[sizeof(wchar_t) * WPOMF_BUFFER_SIZE];   \
+        static wchar_t buffer2[sizeof(wchar_t) * WPOMF_BUFFER_SIZE];  \
+        memset(buffer, 0, sizeof(wchar_t) * WPOMF_BUFFER_SIZE);       \
+        memset(buffer2, 0, sizeof(wchar_t) * WPOMF_BUFFER_SIZE);      \
+        swprintf(buffer, WPOMF_BUFFER_SIZE, L"" format, __VA_ARGS__); \
+        pango_format(buffer2, WPOMF_BUFFER_SIZE, buffer, tags, &s);   \
+        buffer2;                                                      \
+    })
+
+static inline panspan spanfrom(panspan psold, panspan psnew)
+{
+    if(psnew.foreground != psold.foreground)
+        psold.foreground = psnew.foreground;
+
+    if(psnew.background != psold.background)
+        psold.background = psnew.background;
+
+    if(psnew.font_desc != psold.font_desc)
+        psold.font_desc = psnew.font_desc;
+
+    if(psnew.size != psold.size)
+        psold.size = psnew.size;
+
+    if(psnew.weight != psold.weight)
+        psold.weight = psnew.weight;
+
+    if(psnew.style != psold.style)
+        psold.style = psnew.style;
+
+    if(psnew.underline != psold.underline)
+        psold.underline = psnew.underline;
+
+    if(psnew.strikethrough != psold.strikethrough)
+        psold.strikethrough = psnew.strikethrough;
+
+    if(psnew.rise != psold.rise)
+        psold.rise = psnew.rise;
+
+    if(psnew.letter_spacing != psold.letter_spacing)
+        psold.letter_spacing = psnew.letter_spacing;
+
+    if(psnew.scale != psold.scale)
+        psold.scale = psnew.scale;
+
+    return psold;
+}
 
 void pango_format_cstr(
   char* dest, size_t ndest, char* string, pantag tags, panspan* span)
